e8f76812d8d5f45af6e018cc45389d303d3d6401
Alexander-2049 <a.alexander.2049@gmail.com>
2024-08-06 08:29:32 +0300

    Lesson-1 tests

--- lesson-1/index.php
+++ lesson-1/index.php
@@ -1,5 +1,15 @@
 <?php
 $title = "Lesson 1";
+$var = "VAR";
+
+include_once 'testFunction.php';
+
+function test()
+{
+  global $title1;
+  echo $title1;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +23,16 @@ $title = "Lesson 1";
 
 <body>
   <h1><?php echo $title ?></h1>
+  <p>PHP version: <?= phpversion() ?></p>
+  <p>PHP returns a warning if variable is undefined: <?= $example_variable ?></p>
+  <p>String variables are joined by .</p>
+  <p><?php echo 'Example <b><code>' . $var . ' </code></b> variable'; ?></p>
+  <p><?php echo "Or with \" \" quotes <b><code>$var</code></b>"; ?></p>
+  <p>var_dump of variable <code>$x = 5</code>: <?php
+  $x = 5;
+  var_dump($x);
+  ?></p>
+  <p>Include test: <?= testFunction() ?></p>
 </body>
 
 </html>
